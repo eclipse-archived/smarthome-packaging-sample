d034c2b13caa567aa27999cda7504e4a392bd273
Christoph Schauer <christoph@schauer-itconsulting.de>
2016-09-26 13:27:50 +0200

    Fixed name of 3rd party libs in xargs file. Use quotes on -D parameter to define paths with spaces.

--- distro/runtime/concierge/start.sh
+++ distro/runtime/concierge/start.sh
@@ -29,23 +29,23 @@ MAIN=$(find framework -name "org.eclipse.concierge-5.0.0*.jar" | sort | tail -1)
 # Start the framework. Make sure java command is available
 java $JAVA_OPTS \
 	-Dosgi.clean=true \
-	-Dorg.osgi.framework.storage=$BASE_FOLDER/userdata/storage \
+	-Dorg.osgi.framework.storage="$BASE_FOLDER/userdata/storage" \
 	-Dosgi.noShutdown=true \
 	-Declipse.ignoreApp=true \
 	-Dsmarthome.servicepid=org.eclipse \
-	-Dsmarthome.logdir=$BASE_FOLDER/userdata/logs \
-	-Dsmarthome.userdata=$BASE_FOLDER/userdata \
-	-Dsmarthome.servicecfg=$RUNTIME_FOLDER/etc/services.cfg \
-	-Dlogback.configurationFile=$RUNTIME_FOLDER/etc/logback_debug.xml \
-	-Dorg.quartz.properties=$RUNTIME_FOLDER/etc/quartz.properties \
+	-Dsmarthome.logdir="$BASE_FOLDER/userdata/logs" \
+	-Dsmarthome.userdata="$BASE_FOLDER/userdata" \
+	-Dsmarthome.servicecfg="$RUNTIME_FOLDER/etc/services.cfg" \
+	-Dlogback.configurationFile="$RUNTIME_FOLDER/etc/logback_debug.xml" \
+	-Dorg.quartz.properties="$RUNTIME_FOLDER/etc/quartz.properties" \
 	-DmdnsName=smarthome \
-	-Djetty.home=$RUNTIME_FOLDER/ \
+	-Djetty.home="$RUNTIME_FOLDER"/ \
 	-Djetty.etc.config.urls=etc/jetty.xml,etc/jetty-deployer.xml,etc/jetty-selector.xml \
 	-Dorg.osgi.service.http.port=$HTTP_PORT \
 	-Dorg.osgi.service.http.port.secure=$HTTPS_PORT \
-	-Dfelix.fileinstall.dir=$BASE_FOLDER/addons \
+	-Dfelix.fileinstall.dir="$BASE_FOLDER/addons" \
 	-Djava.library.path=lib \
 	-Dfelix.fileinstall.active.level=4 \
 	-Djava.awt.headless=true \
 	-Dfile.encoding="UTF-8" \
-	-jar $MAIN $DIRNAME/smarthome.xargs
+	-jar $MAIN "$DIRNAME/smarthome.xargs"
